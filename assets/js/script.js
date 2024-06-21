27a7a9b47f98b3195ce6f234f31568394477b3a2
pampam01 <emailhpwiko@gmail.com>
2024-06-21 18:24:37 +0700

    add login

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /**
  * navbar variables
@@ -12,18 +12,12 @@ const overlay = document.querySelector("[data-overlay]");
 const navElemArr = [navOpenBtn, navCloseBtn, overlay];
 
 for (let i = 0; i < navElemArr.length; i++) {
-
   navElemArr[i].addEventListener("click", function () {
-
     navbar.classList.toggle("active");
     overlay.classList.toggle("active");
     document.body.classList.toggle("active");
-
   });
-
 }
-
-
 
 /**
  * header sticky
@@ -32,12 +26,12 @@ for (let i = 0; i < navElemArr.length; i++) {
 const header = document.querySelector("[data-header]");
 
 window.addEventListener("scroll", function () {
-
-  window.scrollY >= 10 ? header.classList.add("active") : header.classList.remove("active");
-
+  window.scrollY >= 10
+    ? header.classList.add("active")
+    : header.classList.remove("active");
 });
 
-
+// Fungsi untuk menampilkan kotak dialog
 
 /**
  * go top
@@ -46,7 +40,20 @@ window.addEventListener("scroll", function () {
 const goTopBtn = document.querySelector("[data-go-top]");
 
 window.addEventListener("scroll", function () {
-
-  window.scrollY >= 500 ? goTopBtn.classList.add("active") : goTopBtn.classList.remove("active");
-
+  window.scrollY >= 500
+    ? goTopBtn.classList.add("active")
+    : goTopBtn.classList.remove("active");
 });
+// Fungsi untuk membuat dan menampilkan modal login
+
+document
+  .querySelector(".play-btn, .play-btn-login")
+  .addEventListener("click", function () {
+    document.getElementById("login").style.display = "block";
+  });
+
+document.querySelector(".close").addEventListener("click", function () {
+  document.getElementById("login").style.display = "none";
+});
+
+// Memanggil fungsi untuk membuat dan menampilkan modal
